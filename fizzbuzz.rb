7ece76510d9f6c26f378368fca50d146df86b69c
Angelina Montanez <angelinamontanez80@gmail.com>
2019-01-31 00:52:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,14 @@
 def fizzbuzz(int)
-  if int% 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
-  end
+
+if int % 15 == 0
+   "FizzBuzz"
+
+ elsif int % 5 == 0
+     "Buzz"
+ elsif int % 3 = 0
+   "Fizz"
+ else
+  nil
+
+end
+ end
